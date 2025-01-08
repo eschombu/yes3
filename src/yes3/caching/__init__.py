b68f6405f959c05feee4879ea13f94709db03b5b
Erik Schomburg <eschomburg@gmail.com>
2025-01-08 17:43:09 -0500

    make CacheCoreMethods available to import from caching module

--- src/yes3/caching/__init__.py
+++ src/yes3/caching/__init__.py
@@ -1,3 +1,3 @@
-from .base import Cache, CacheNotInitializedError, MultiCache, Serializer
+from .base import Cache, CacheCoreMethods, CacheNotInitializedError, MultiCache, Serializer
 from .local import LocalDiskCache
 from .cache import S3Cache
