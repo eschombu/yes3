36e7c3e4f9d7c1997a19fa8a51b2b0bcedf2acfd
Erik Schomburg <eschomburg@flatironinstitute.org>
2025-01-09 14:48:47 -0500

    rename local.py and cache.py in caching/ to local_cache.py and s3_cache.py

--- src/yes3/caching/__init__.py
+++ src/yes3/caching/__init__.py
@@ -1,3 +1,3 @@
 from .base import Cache, CacheCoreMethods, CacheNotInitializedError, MultiCache, Serializer
-from .local import LocalDiskCache
-from .cache import S3Cache
+from .local_cache import LocalDiskCache
+from .s3_cache import S3Cache
