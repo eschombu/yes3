83bcf2282dae3f77ae17ff0eade9cd5e96cfda98
Erik Schomburg <eschomburg@gmail.com>
2025-01-16 17:44:50 -0500

    caches no longer have initialization state or methods

--- src/yes3/caching/local_cache.py
+++ src/yes3/caching/local_cache.py
@@ -16,6 +16,7 @@ class PickleSerializer(Serializer):
             return pickle.load(f)
 
     def write(self, path, obj):
+        os.makedirs(os.path.dirname(os.path.abspath(path)), exist_ok=True)
         with open(path, 'wb') as f:
             pickle.dump(obj, f)
 
@@ -80,57 +81,42 @@ class LocalDiskCache(Cache):
     @staticmethod
     def _build_catalog_dict(reader_writer: LocalReaderWriter) -> dict:
         catalog_dict = {}
-        for (dirpath, dirnames, filenames) in os.walk(reader_writer.path):
-            for fname in filenames:
-                fpath = Path(dirpath) / fname
-                key = reader_writer.path2key(fpath)
-                if key in catalog_dict:
-                    raise KeyError(f"Key already in cache catalog: '{key}'")
-                catalog_dict[key] = fpath
+        if os.path.exists(reader_writer.path):
+            for (dirpath, dirnames, filenames) in os.walk(reader_writer.path):
+                for fname in filenames:
+                    fpath = Path(dirpath) / fname
+                    key = reader_writer.path2key(fpath)
+                    if key in catalog_dict:
+                        raise KeyError(f"Key already in cache catalog: '{key}'")
+                    catalog_dict[key] = fpath
         if len(catalog_dict.keys()) > 0:
             print(f'{len(catalog_dict.keys())} cached items discovered at {reader_writer.path}')
         return catalog_dict
 
     @classmethod
-    def create(cls, path: str | Path, serializer: str | Serializer = 'pkl', initialize=True, **kwargs):
+    def create(cls, path: str | Path, serializer: str | Serializer = 'pkl', **kwargs):
         reader_writer = LocalReaderWriter(path, serializer)
         catalog_builder = partial(cls._build_catalog_dict, reader_writer=reader_writer)
-        catalog = CachePathDictCatalog(initialize=initialize, catalog_builder=catalog_builder)
-        return cls(catalog, reader_writer, initialize=initialize, **kwargs)
+        catalog = CachePathDictCatalog(catalog_builder=catalog_builder)
+        return cls(catalog, reader_writer, **kwargs)
 
     @property
     def path(self) -> Path:
         return self._reader_writer.path
 
-    def subcache(self, rel_path: str, initialize=None) -> Self:
+    def subcache(self, rel_path: str) -> Self:
         path = self.path / rel_path
         kwargs = dict(active=self.is_active(), read_only=self.is_read_only())
-        if initialize is not None:
-            kwargs['initialize'] = initialize
         return type(self).create(path, self._reader_writer._serializer, **kwargs)
 
-    def initialize(self) -> Self:
-        if not self.is_initialized():
-            print(f"Initializing cache at {self.path}")
-            if not self.path.exists():
-                os.makedirs(self.path, exist_ok=True)
-            self._catalog.initialize()
-        else:
-            print(f"Cache initialized at {self.path}")
-        return self
-
-    def is_initialized(self) -> bool:
-        return super().is_initialized() and self.path.exists()
-
-    def clear(self, force=False, initialize=False) -> Self:
+    def clear(self, force=False) -> Self:
         if self.is_active() and self._catalog and self.path.exists():
             if not force:
                 raise RuntimeError(f'Clearing this cache ({self.path}) requires specifying force=True')
             print(f'Deleting {len(self.keys())} item(s) from cache at {self.path}')
             shutil.rmtree(self.path)
-            new_cache = type(self).create(self.path, self._reader_writer._serializer, initialize=initialize)
-            self.__init__(new_cache._catalog, new_cache._reader_writer, active=self._active, initialize=initialize,
-                          read_only=self._read_only)
+            new_cache = type(self).create(self.path, self._reader_writer._serializer)
+            self.__init__(new_cache._catalog, new_cache._reader_writer, active=self._active, read_only=self._read_only)
         return self
 
     def _repr_params(self) -> list[str]:
