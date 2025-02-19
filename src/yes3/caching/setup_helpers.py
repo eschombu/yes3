1aa8e6bd0ab2577fce175125830fa6b05e86c1c7
Erik Schomburg <eschomburg@flatironinstitute.org>
2025-02-19 09:41:16 -0500

    add MemoryCache

--- src/yes3/caching/setup_helpers.py
+++ src/yes3/caching/setup_helpers.py
@@ -1,24 +1,36 @@
+from imp import cache_from_source
 from pathlib import Path
+from typing import Optional
 
 from yes3.s3 import is_s3_url, S3Location
-from yes3.caching import CacheCore, LocalDiskCache, MultiCache, S3Cache
+from yes3.caching import CacheCore, LocalDiskCache, MemoryCache, MultiCache, S3Cache
 
 
-def setup_single_cache(path: str | Path | S3Location | CacheCore, rebuild_missing_metadata=False) -> CacheCore:
+def setup_single_cache(
+        path: Optional[str | Path | S3Location | CacheCore] = None,
+        in_memory=False,
+        rebuild_missing_metadata=False,
+) -> CacheCore:
     if isinstance(path, CacheCore):
         cache = path
     elif isinstance(path, S3Location) or (isinstance(path, str) and is_s3_url(path)):
         cache = S3Cache.create(path, rebuild_missing_meta=rebuild_missing_metadata)
     elif isinstance(path, Path) or isinstance(path, str):
         cache = LocalDiskCache.create(path, rebuild_missing_meta=rebuild_missing_metadata)
-    else:
+    elif not in_memory:
         raise TypeError('`path` must be a Cache, local path, or s3 location')
+    else:
+        cache = MemoryCache()
     return cache
 
 
-def setup_cache(*paths, sync=False, rebuild_missing_metadata=False) -> CacheCore | None:
-    caches = [setup_single_cache(path, rebuild_missing_metadata=rebuild_missing_metadata)
-              for path in paths if path is not None]
+def setup_cache(*paths, in_memory=False, sync=False, rebuild_missing_metadata=False) -> CacheCore | None:
+    caches = []
+    if in_memory:
+        caches.append(setup_single_cache(in_memory=in_memory))
+    for path in paths:
+        if path is not None:
+            caches.append(setup_single_cache(path, rebuild_missing_metadata=rebuild_missing_metadata))
     if len(caches) == 0:
         return None
     elif len(caches) == 1:
